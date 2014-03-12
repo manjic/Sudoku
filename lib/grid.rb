8249df98d3d929ae79b03751de69b9580a4fa5a2
Manjit Singh <manjitsingh23@gmail.com>
2014-03-12 13:49:29 +0000

    Passes first 2 tests- grid is 81 cells and first cell value is correct

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,6 +1,23 @@
 class Grid
 
- def initialize
-	end
-  
+attr_reader :cells
+
+def initialize(puzzle)
+  @cells = puzzle.chars.map {|n| (n.to_i)}
 end
+
+# def cells
+  
+# end
+
+ 
+# def solve
+# end
+
+# def solved?
+# end
+
+# def inspect
+#   end
+end
+
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -1,8 +1,25 @@
 require_relative "../lib/grid"
 
+
 describe Grid do
 
-    it "should initialize with an empty grid of size 9x9" do
+    context "initialization" do
+
+      let(:puzzle) {'015003002000100906270068430490002017501040380003905000900081040860070025037204600'} 
+    
+      let(:grid) {Grid.new(puzzle)}
+
+      it "should have 81 cells" do
+        expect(grid.cells.length).to  eq(81)
+      end
+
+    it 'should have an unsolved first cell' do
+        expect(grid.cells.first).to eq(0)
     end
 
+    xit it 'should have a solved second cell with value 1' do
+      expect(grid.cells.second).to eq(1)
+    end
+
+    end
 	end
